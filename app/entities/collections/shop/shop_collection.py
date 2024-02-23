b00fd60e39de1c07c964d49a16d7bb7c92064b51
bhnvx <xzt______@naver.com>
2024-02-23 10:12:39 +0900

    Add TEST CODE About distinct

--- app/entities/collections/shop/shop_collection.py
+++ app/entities/collections/shop/shop_collection.py
@@ -1,8 +1,10 @@
 from dataclasses import asdict
+from typing import Any
 
 from motor.motor_asyncio import AsyncIOMotorCollection
 
 from app.entities.category.category_codes import CategoryCode
+from app.entities.collections.geo_json import GeoJsonPoint, GeoJsonPolygon
 from app.entities.collections.shop.shop_document import (
     ShopDeliveryAreaSubDocument,
     ShopDocument,
@@ -34,3 +36,34 @@ class ShopCollection:
             category_codes=category_codes,
             delivery_areas=delivery_areas,
         )
+
+    @classmethod
+    async def point_intersects(cls, point: GeoJsonPoint) -> list[ShopDocument]:
+        return [
+            cls._parse(result)
+            for result in await cls._collection.find(
+                {"delivery_areas.poly": {"$geoIntersects": {"$geometry": asdict(point)}}}
+            ).to_list(length=None)
+        ]
+
+    @classmethod
+    def _parse(cls, result: dict[Any, Any]) -> ShopDocument:
+        return ShopDocument(
+            _id=result["_id"],
+            name=result["name"],
+            delivery_areas=[
+                ShopDeliveryAreaSubDocument(poly=GeoJsonPolygon(coordinates=delivery_area["poly"]["coordinates"]))
+                for delivery_area in result["delivery_areas"]
+            ],
+            category_codes=[CategoryCode(category_code) for category_code in result["category_codes"]],
+        )
+
+    @classmethod
+    async def get_distinct_category_codes_by_point_intersects(cls, point: GeoJsonPoint) -> list[CategoryCode]:
+        return [
+            CategoryCode(category_code)
+            for category_code in await cls._collection.distinct(
+                "category_codes",
+                {"delivery_areas.poly": {"$geoIntersects": {"$geometry": asdict(point)}}},
+            )
+        ]
